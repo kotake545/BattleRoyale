4f2a101e29d69d1ec8066dbf17650e17be0a08d4
tyoku <tyoku@tyoku-PC>
2013-04-28 18:07:40 +0900

    初回座標設定完了

--- battleroyale/src/com/tyoku/BattleRoyale.java
+++ battleroyale/src/com/tyoku/BattleRoyale.java
@@ -1,5 +1,6 @@
 package com.tyoku;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginManager;
@@ -11,19 +12,24 @@ import com.tyoku.listener.BRPlayerListener;
 
 public class BattleRoyale extends JavaPlugin{
 	private Logger log;
+	private File config;
 	@SuppressWarnings("unused")
 	private DBManager dbm = new DBManager("battleroyale.sqlite3");
-	public final BRPlayerListener pl = new BRPlayerListener();
 
 	@Override
 	public void onEnable() {
 		this.log = this.getLogger();
 		this.log.info("BattleRoyale configre preparing....");
+		this.config = new File(this.getDataFolder(), "config.yml");
+		if (!config.exists()) {
+		    this.saveDefaultConfig();
+		}
 
+		this.log.info("BattleRoyale commands preparing....");
 		this.getCommand("warp").setExecutor(new StartPosCmd(this));
 
 		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvents(pl, this);
+		pm.registerEvents(new BRPlayerListener(this), this);
 
 		this.log.info("BattleRoyale Enabled.");
 	}
--- battleroyale/src/com/tyoku/listener/BRPlayerListener.java
+++ battleroyale/src/com/tyoku/listener/BRPlayerListener.java
@@ -3,7 +3,9 @@ package com.tyoku.listener;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,16 +13,42 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
+import com.tyoku.BattleRoyale;
+
 public class BRPlayerListener implements Listener {
-	@SuppressWarnings("unused")
 	private Logger log;
+	private BattleRoyale plugin;
+
+	public BRPlayerListener(BattleRoyale battleRoyale) {
+		this.plugin = battleRoyale;
+		this.log = battleRoyale.getLogger();
+	}
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 	    Player player = event.getPlayer(); // Joinしたプレイヤー
 	    PlayerInventory inventory = player.getInventory(); // プレイヤーのインベントリ
 
-        player.sendMessage(ChatColor.GOLD + "やっほおおお！！");
+	    //プリセット位置へプレイヤーを飛ばす
+	    int x = this.plugin.getConfig().getInt("roompos.x");
+	    int y = this.plugin.getConfig().getInt("roompos.y");
+	    int z = this.plugin.getConfig().getInt("roompos.z");
+
+	    if(y == -100){
+		    x = player.getLocation().getBlockX();
+		    y = player.getLocation().getBlockY();
+		    z = player.getLocation().getBlockZ();
+		    this.plugin.getConfig().set("roompos.x",x);
+		    this.plugin.getConfig().set("roompos.y",y);
+		    this.plugin.getConfig().set("roompos.z",z);
+		    this.plugin.saveConfig();
+	    }
+	    World w = player.getWorld();
+	    Location nLoc = new Location(w, x, y, z);
+	    this.log.info(String.format("プレイヤーを(X:%d Y:%d Z:%d)へ転送", x,y,z));
+        player.teleport(nLoc);
+        player.sendMessage(ChatColor.GOLD + "バトロワへようこそ！");
+
 	    if (!inventory.contains(Material.DIAMOND)) {
 	        inventory.addItem(new ItemStack(Material.DIAMOND, 64)); // プレイヤーインベントリに山積みのダイヤモンドを加える
 	        player.sendMessage(ChatColor.GOLD + "よく来たな!もっとダイヤモンドをくれてやろう、このとんでもない成金め!!");
