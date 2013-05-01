d09c7bc284b345121061175307dcd007a3f88ad2
tyoku <tyoku@tyoku-PC>
2013-05-02 00:58:59 +0900

    微調整

--- battleroyale/src/com/tyoku/BattleRoyale.java
+++ battleroyale/src/com/tyoku/BattleRoyale.java
@@ -21,118 +21,119 @@ import com.tyoku.listener.BRPlayerListener;
 import com.tyoku.listener.MapListener;
 import com.tyoku.util.BRUtils;
 
-public class BattleRoyale extends JavaPlugin{
-        private Logger log;
-        private File config;
-        private BRManager brManager;
-        private Map<String, BRPlayer> playerStat;
-        private Map<String,BukkitTask> playerTask;
-        private List<String> randomMapBlocks;
-        private List<String> deadAreaBlocks;
-        private List<String> nextAreaBlocks;
-        private BukkitTask createZoneTask;
+public class BattleRoyale extends JavaPlugin {
+	private Logger log;
+	private File config;
+	private BRManager brManager;
+	private Map<String, BRPlayer> playerStat;
+	private Map<String, BukkitTask> playerTask;
+	private List<String> randomMapBlocks;
+	private List<String> deadAreaBlocks;
+	private List<String> nextAreaBlocks;
+	private BukkitTask createZoneTask;
 
-        @SuppressWarnings("unused")
-        private DBManager dbm = new DBManager("battleroyale.sqlite3");
+	@SuppressWarnings("unused")
+	private DBManager dbm = new DBManager("battleroyale.sqlite3");
 
-        @Override
-        public void onEnable() {
-                this.log = this.getLogger();
-                this.log.info("BattleRoyale configre preparing....");
-                this.config = new File(this.getDataFolder(), "config.yml");
-                if (!config.exists()) {
-                    this.saveDefaultConfig();
-                }
-                setBrManager(new BRManager());
-
-                this.randomMapBlocks = BRUtils.getRundumMRMapBlocks();
-                nextAreaBlocks = new ArrayList<String>();
-                deadAreaBlocks = new ArrayList<String>();
-
-                this.playerStat = new HashMap<String, BRPlayer>();
-                this.setPlayerTask(new HashMap<String, BukkitTask>());
-
-                //コマンド登録
-                this.log.info("BattleRoyale commands preparing....");
-                this.getCommand("setroom").setExecutor(new StartPosCmd(this));
-                this.getCommand("brgame").setExecutor(new BrGame(this));
-                this.getCommand("setbrarea").setExecutor(new GameArea(this));
-
-                //リスナー登録
-                PluginManager pm = this.getServer().getPluginManager();
-                pm.registerEvents(new BRPlayerListener(this), this);
-                pm.registerEvents(new MapListener(this), this);
-
-                this.log.info("BattleRoyale Enabled.");
-        }
-
-        @Override
-        public void onDisable() {
-        	if(this.createZoneTask != null){
-        		this.createZoneTask.cancel();
-        		this.createZoneTask = null;
-        	}
-                this.log.info("BattleRoyale Disabled.");
-        }
-
-        public BRManager getBrManager() {
-                return brManager;
-        }
-
-        public void setBrManager(BRManager manager) {
-                this.brManager = manager;
-        }
-
-        public Map<String, BRPlayer> getPlayerStat() {
-                return playerStat;
-        }
-
-        public void setPlayerStat(Map<String, BRPlayer> playerStat) {
-                this.playerStat = playerStat;
-        }
-
-        public Map<String,BukkitTask> getPlayerTask() {
-                return playerTask;
-        }
-
-        public void setPlayerTask(Map<String,BukkitTask> playerTask) {
-                this.playerTask = playerTask;
-        }
-        public List<String> getRandomMapBlocks() {
-                return randomMapBlocks;
-        }
-
-        public void setRandomMapBlocks(List<String> randomMapBlocks) {
-                this.randomMapBlocks = randomMapBlocks;
-        }
-
-        public List<String> getDeadAreaBlocks() {
-                return deadAreaBlocks;
-        }
-
-        public void setDeadAreaBlocks(List<String> deadAreaBlocks) {
-                this.deadAreaBlocks = deadAreaBlocks;
-        }
-
-        /**
-         * @return nextAreaBlocks
-         */
-        public List<String> getNextAreaBlocks() {
-            return nextAreaBlocks;
-        }
-
-        /**
-         * @param nextAreaBlocks セットする nextAreaBlocks
-         */
-        public void setNextAreaBlocks(List<String> nextAreaBlocks) {
-            this.nextAreaBlocks = nextAreaBlocks;
-        }
-
-		public BukkitTask getCreateZoneTask() {
-			return createZoneTask;
+	@Override
+	public void onEnable() {
+		this.log = this.getLogger();
+		this.log.info("BattleRoyale configre preparing....");
+		this.config = new File(this.getDataFolder(), "config.yml");
+		if (!config.exists()) {
+			this.saveDefaultConfig();
 		}
+		setBrManager(new BRManager());
 
-		public void setCreateZoneTask(BukkitTask createZoneTask) {
-			this.createZoneTask = createZoneTask;
+		this.randomMapBlocks = BRUtils.getRundumMRMapBlocks();
+		nextAreaBlocks = new ArrayList<String>();
+		deadAreaBlocks = new ArrayList<String>();
+
+		this.playerStat = new HashMap<String, BRPlayer>();
+		this.setPlayerTask(new HashMap<String, BukkitTask>());
+
+		//コマンド登録
+		this.log.info("BattleRoyale commands preparing....");
+		this.getCommand("setroom").setExecutor(new StartPosCmd(this));
+		this.getCommand("brgame").setExecutor(new BrGame(this));
+		this.getCommand("setbrarea").setExecutor(new GameArea(this));
+
+		//リスナー登録
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvents(new BRPlayerListener(this), this);
+		pm.registerEvents(new MapListener(this), this);
+
+		this.log.info("BattleRoyale Enabled.");
+	}
+
+	@Override
+	public void onDisable() {
+		if (this.createZoneTask != null) {
+			this.createZoneTask.cancel();
+			this.createZoneTask = null;
 		}
+		this.log.info("BattleRoyale Disabled.");
+	}
+
+	public BRManager getBrManager() {
+		return brManager;
+	}
+
+	public void setBrManager(BRManager manager) {
+		this.brManager = manager;
+	}
+
+	public Map<String, BRPlayer> getPlayerStat() {
+		return playerStat;
+	}
+
+	public void setPlayerStat(Map<String, BRPlayer> playerStat) {
+		this.playerStat = playerStat;
+	}
+
+	public Map<String, BukkitTask> getPlayerTask() {
+		return playerTask;
+	}
+
+	public void setPlayerTask(Map<String, BukkitTask> playerTask) {
+		this.playerTask = playerTask;
+	}
+
+	public List<String> getRandomMapBlocks() {
+		return randomMapBlocks;
+	}
+
+	public void setRandomMapBlocks(List<String> randomMapBlocks) {
+		this.randomMapBlocks = randomMapBlocks;
+	}
+
+	public List<String> getDeadAreaBlocks() {
+		return deadAreaBlocks;
+	}
+
+	public void setDeadAreaBlocks(List<String> deadAreaBlocks) {
+		this.deadAreaBlocks = deadAreaBlocks;
+	}
+
+	/**
+	 * @return nextAreaBlocks
+	 */
+	public List<String> getNextAreaBlocks() {
+		return nextAreaBlocks;
+	}
+
+	/**
+	 * @param nextAreaBlocks セットする nextAreaBlocks
+	 */
+	public void setNextAreaBlocks(List<String> nextAreaBlocks) {
+		this.nextAreaBlocks = nextAreaBlocks;
+	}
+
+	public BukkitTask getCreateZoneTask() {
+		return createZoneTask;
+	}
+
+	public void setCreateZoneTask(BukkitTask createZoneTask) {
+		this.createZoneTask = createZoneTask;
+	}
 }
